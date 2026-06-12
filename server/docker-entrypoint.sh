be5e4cebef3abf04c790172bbea997f1dff6a466
jamba <ytmusic4883@gmail.com>
2026-06-12 13:05:20 +0300

    fix entrypoint: use node crypto instead of openssl

--- server/docker-entrypoint.sh
+++ server/docker-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 if [ -z "$JWT_SECRET" ]; then
-  export JWT_SECRET=$(openssl rand -base64 48)
+  export JWT_SECRET=$(node -e "console.log(require('crypto').randomBytes(48).toString('base64'))")
 fi
 if [ -z "$JWT_REFRESH_SECRET" ]; then
-  export JWT_REFRESH_SECRET=$(openssl rand -base64 48)
+  export JWT_REFRESH_SECRET=$(node -e "console.log(require('crypto').randomBytes(48).toString('base64'))")
 fi
 exec node dist/app.js
